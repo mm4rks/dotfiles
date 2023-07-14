7869e8d49969275a2f13194c9f4b7eb30057b76d
Marvin Marks <marvin.marks@posteo.de>
2023-07-14 11:37:46 +0200

    Install telescope native fzf

--- nvim/.config/nvim/lua/mm4rks/packer.lua
+++ nvim/.config/nvim/lua/mm4rks/packer.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use({
         'rose-pine/neovim',
         as = 'rose-pine',
