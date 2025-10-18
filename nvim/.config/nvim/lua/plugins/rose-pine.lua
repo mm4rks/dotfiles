a52398b68237404d7bb9665dfdf61c5e616b6c9d
Marvin Marks <65773903+mm4rks@users.noreply.github.com>
2025-10-18 03:30:40 +0200

    Update rose-pine.lua

--- nvim/.config/nvim/lua/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/plugins/rose-pine.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		-- Call setup with your options
 		require("rose-pine").setup({
-			disable_background = true,
+			--disable_background = true,
 			disable_italics = true,
 		})
 
