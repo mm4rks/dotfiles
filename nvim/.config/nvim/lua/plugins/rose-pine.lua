7b50da46b3e799bb0656cca03e37790d3586057f
Marvin Marks <65773903+mm4rks@users.noreply.github.com>
2025-10-18 03:17:11 +0200

    Create rose-pine.lua

---
+++ nvim/.config/nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,15 @@
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	priority = 1000, -- Ensures this plugin loads first
+	config = function()
+		-- Call setup with your options
+		require("rose-pine").setup({
+			disable_background = true,
+			disable_italics = true,
+		})
+
+		-- Set the colorscheme
+		vim.cmd("colorscheme rose-pine")
+	end
+}
