dfde774a071c732bb53975c3f8cbd7a8c89ab262
Marvin Marks <mm4rks@posteo.de>
2026-01-31 20:03:15 +0100

    mise changes

--- user_setup.sh
+++ user_setup.sh
@@ -64,6 +64,7 @@ configure_mise() {
 
 # --- Main Execution ---
 main() {
+    eval "$(mise activate bash)"
     if [ "$EUID" -eq 0 ]; then
         error "This script should be run as a regular user, not as root."
     fi
